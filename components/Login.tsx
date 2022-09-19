780e0b5bda0b8bc22d921734fdea9fb8ffd3587f
@vishal.kumar <vishal.kumar@growthnatives.com>
2022-09-19 07:52:57 +0530

    added stylelint and edit other config filed

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import { Mail, Lock } from '@mui/icons-material';
 import { FieldValues, useForm } from 'react-hook-form';
+import { Mail, Lock } from '@mui/icons-material';
 import Input from '@core/Input';
 import Button from '@core/Button';
+import styles from '@styles/components/Login.module.scss';
 
 const Login: FunctionComponent = (): JSX.Element => {
   const { control, handleSubmit } = useForm<FieldValues, any>();
@@ -13,17 +14,33 @@ const Login: FunctionComponent = (): JSX.Element => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input name="email" InputType="email" control={control} label={'Email'} required />
-        <Input name="Password" InputType="password" control={control} label={'Password'} required />
-        <Button
-          onClick={() => console.log('hello')}
-          label="Submit"
-          variant="contained"
-          btnSize="large"
-          type="submit"
-        />
-      </form>
+      <div className={styles.login_page_container}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            name="email"
+            InputType="email"
+            control={control}
+            label={'Email'}
+            required
+            startIcon={{ icon: <Mail />, position: 'start', shouldInclude: true }}
+          />
+          <Input
+            name="Password"
+            InputType="password"
+            control={control}
+            label={'Password'}
+            required
+            startIcon={{ icon: <Lock />, position: 'start', shouldInclude: true }}
+          />
+          <Button
+            onClick={() => console.log('hello')}
+            label="Submit"
+            variant="contained"
+            btnSize="large"
+            type="submit"
+          />
+        </form>
+      </div>
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import '@styles/variables.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="theme_light">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
